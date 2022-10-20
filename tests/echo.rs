6a0267d73d545d9b0fe421a56fae6c45b765e36f
avery <avrypromo@pm.me>
2022-10-19 19:12:14 -0700

    unit testing with echo, pkgbuild script?

--- tests/echo.rs
+++ tests/echo.rs
@@ -1,18 +1,12 @@
 use std::process::Command;
 
-pub struct Pacman;
-impl churn::Pkgmgr for Pacman {
-    const NAME: &'static str = "pacman";
+pub struct Echo;
+impl churn::Pkgmgr for Echo {
+    const NAME: &'static str = "echo";
 
-    // lots of lines for "pkgfile -b $pkg | head -n 1"
     fn get_pkg_name(&self, supplied_cmd: &String) -> String {
         String::from_utf8( Command::new("pkgfile")
-            // -b filters pkgfile results for binaries
-            // in my experience, this usually filters the
-            // bash completion files that "pacman -Fq" doesn't catch
-            // pkgfile also seems to be faster. space/time tradeoff?
-            // inevitably some edge case will ruin this
-            .args(["-b", supplied_cmd])
+            .arg(supplied_cmd)
             .output()
             .unwrap()
             .stdout
@@ -26,14 +20,14 @@ impl churn::Pkgmgr for Pacman {
 
     fn install(&self, pkg_name: &String) -> std::process::Output {
         Command::new(Self::NAME)
-            .args(["--sync", "--noconfirm", &pkg_name])
+            .arg(&pkg_name)
             .output()
             .unwrap()
     }
 
     fn uninstall(&self, pkg_name: &String) -> std::process::Output {
         Command::new(Self::NAME)
-            .args(["--remove", "--noconfirm", &pkg_name])
+            .arg(&pkg_name)
             .output()
             .unwrap()
     }
